d7d30305ef0d3111e0417446299980f4393a7b19
Venkateshsandupatla <66814938+Venkateshsandupatla@users.noreply.github.com>
2021-04-29 13:32:26 +0530

    Add files via upload

---
+++ chatudp.py
@@ -0,0 +1,42 @@
+import socket
+import os
+#import time
+import threading
+# udp protocal
+protocal = socket.SOCK_DGRAM
+
+# network address family
+afn = socket.AF_INET
+
+#this is our socket
+s = socket.socket(afn,protocal)
+
+ip = "192.168.1.13"
+port = 1234
+
+# binding our socket with ip and port
+s.bind( (ip,port) )
+
+recvip = input("enter reciever ip :")
+print("\t\t\t\t\ chat app" )
+print("you are connected to : ",recvip)
+
+
+def recv():
+    while True:
+        xr = s.recvfrom(1024)
+        clientip = xr[1][0]
+        data = xr[0].decode() 
+        
+        print("\t\t message:  " + data +"\n")
+#       os.system("tput setaf 0")
+def send():
+    while True :
+        xs = input("enter message to send: ")
+        s.sendto(xs.encode() , (recvip,port))
+                
+t1 = threading.Thread(target= recv)
+t2 = threading.Thread(target= send)
+
+t1.start()
+t2.start()
